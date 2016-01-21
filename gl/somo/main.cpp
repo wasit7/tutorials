a116780bb540fe2ed50135f52492562ebeb7a5ff
wasit7 <wasit7@gmail.com>
2016-01-21 23:09:46 +0700

    somo collision

--- gl/somo/main.cpp
+++ gl/somo/main.cpp
@@ -4,131 +4,158 @@
 #include <time.h>
 #include <math.h>
 
-#define PI 3.14159265358979323846
-#define dt 0.1
+#define PI 3.14159265358979323846f
+#define dt 0.1f
+#define N 200
+#define WIDTH 1000
+#define HEIGHT 600
 using namespace std;
 
-struct ball {
-    short mass;
-    float position[2];
-    float velocity[2];
-    float color[3];
+class myObj{
+public:
+    float m,r,sx,sy,vx,vy,cr,cg,cb;
+    myObj(){
+        this->m  = float(rand() % 500 + 100);
+        this->r  = sqrt(this->m/PI);
+        this->sx = float(rand() % WIDTH);
+        this->sy = float(rand() % HEIGHT);
+        this->vx = ((rand() % 100) - 50) / 10.0f;
+        this->vy = ((rand() % 100) - 50) / 10.0f;
+        this->cr = (rand() % 256) / 255.0f;
+        this->cg = (rand() % 256) / 255.0f;
+        this->cb = (rand() % 256) / 255.0f;
+    }
+    ~myObj(){
+        cout<<"deconstruct m:"<<this->m<<endl;
+    }
+    void draw();
+    void update();
+    void update(float sx, float sy, float vx, float vy);
+    void print();
 };
-
-ball Balls[10];
-bool once = true;
-
-void drawCircle(float x, float y, float radius) {
-    int i;
+myObj balls[N];
+void myObj::draw(){
     int triangleAmount = 20;
     float twicePi = 2.0f * PI;
+    glColor3f(this->cr,this->cg,this->cb);
     glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(x, y);
-    for (i = 0; i <= triangleAmount; i++) {
-        glVertex2f(x + (radius * cos(i *  twicePi / triangleAmount)), y + (radius * sin(i * twicePi / triangleAmount)));
+    glVertex2f(this->sx, this->sy);
+    for (int i = 0; i <= triangleAmount; i++) {
+        glVertex2f(
+            this->sx + (this->r * cos(i * twicePi / triangleAmount)),
+            this->sy + (this->r * sin(i * twicePi / triangleAmount))
+        );
     }
     glEnd();
 }
-
-void moveCircle(ball &ball, short ballNum) {
-    for (short m = 0; m < 9; ++m) {
-        for (short n = m+1; n < 10; ++n) {
-            float distanceBtBalls = sqrt(pow(Balls[m].position[0] - Balls[n].position[0], 2) + pow(Balls[m].position[1] - Balls[n].position[1], 2));
-            //cout<<"m,n: "<< m <<"," << n << endl;
-            if (distanceBtBalls < Balls[m].mass + Balls[n].mass) {
-                float un[2];
-                float ut[2];
-                un[0] = (Balls[m].position[0] - Balls[n].position[0]) / distanceBtBalls;
-                un[1] = (Balls[m].position[1] - Balls[n].position[1]) / distanceBtBalls;
-                Balls[m].position[0]+=0.5*un[0]*(
-                            Balls[m].mass + Balls[n].mass-distanceBtBalls
-                        );
-                Balls[m].position[1]+=0.5*un[1]*(
-                            Balls[m].mass + Balls[n].mass-distanceBtBalls
-                        );
-                Balls[n].position[0]-=0.5*un[0]*(
-                            Balls[m].mass + Balls[n].mass-distanceBtBalls
-                        );
-                Balls[n].position[1]-=0.5*un[1]*(
-                            Balls[m].mass + Balls[n].mass-distanceBtBalls
-                        );
-                ut[0] = -un[1]; ut[1] = un[0];
-
-                //cout<<"distanceBtBalls: "<<distanceBtBalls<<endl;
-                //cout<<"sqrt(un[0]*un[0]+un[1]*un[1]): "<<sqrt(un[0]*un[0]+un[1]*un[1])<<endl;
-                //cout<<"sqrt(ut[0]*ut[0]+ut[1]*ut[1]): "<<sqrt(ut[0]*ut[0]+ut[1]*ut[1])<<endl;
-                //cout<<"sqrt(un[0]*ut[0]+un[1]*ut[1]): "<<sqrt(un[0]*ut[0]+un[1]*ut[1])<<endl;
-                float v1n = (Balls[m].velocity[0] * un[0]) + (Balls[m].velocity[1] * un[1]);
-                float v1t = (Balls[m].velocity[0] * ut[0]) + (Balls[m].velocity[1] * ut[1]);
-                float v2n = (Balls[n].velocity[0] * un[0]) + (Balls[n].velocity[1] * un[1]);
-                float v2t = (Balls[n].velocity[0] * ut[0]) + (Balls[n].velocity[1] * ut[1]);
-                v1n = (v1n * (Balls[m].mass - Balls[n].mass) + (2 * Balls[n].mass * v2n)) / (Balls[m].mass + Balls[n].mass);
-                v2n = (v2n * (Balls[n].mass - Balls[m].mass) + (2 * Balls[m].mass * v1n)) / (Balls[m].mass + Balls[n].mass);
-                Balls[m].velocity[0] = (v1n * un[0]) + (v1t * ut[0]);
-                Balls[m].velocity[1] = (v1n * un[1]) + (v1t * ut[1]);
-                Balls[n].velocity[0] = (v2n * un[0]) + (v2t * ut[0]);
-                Balls[n].velocity[1] = (v2n * un[1]) + (v2t * ut[1]);
-            }
-        }
+void myObj::update(void){
+    this->sx+=dt*this->vx;
+    this->sy+=dt*this->vy;
+}
+void myObj::update(float sx, float sy, float vx, float vy){
+    cout<<"debug: update(sx,sy,vx,vy)"<<endl;
+}
+void myObj::print(){
+    cout<<"m:"<<this->m<<" r:"<<this->r
+        <<" sx:"<<this->sx<<" sy:"<<this->sy
+        <<" vx:"<<this->vx<<" vy:"<<this->vy
+        //<<" cr:"<<this->cr<<" cg:"<<this->cg<<" cb:"<<this->cb
+        <<endl;
+}
+void init(){
+    srand((unsigned int)time(NULL));
+}
+void collideWall(myObj &A){
+    if(A.sx-A.r<0){
+        A.vx*=-1.0f;
+        A.sx=A.r;
     }
-    for (short m = 0; m < 10; ++m) {
-        Balls[m].position[0] = Balls[m].position[0] + Balls[m].velocity[0]*dt;
-        Balls[m].position[1] = Balls[m].position[1] + Balls[m].velocity[1]*dt;
-        if (Balls[m].position[0] - Balls[m].mass <= 0) {
-            Balls[m].velocity[0] = abs(Balls[m].velocity[0]);
-        }
-        else if (Balls[m].position[0] + Balls[m].mass >= 1000){
-            Balls[m].velocity[0] = -1.0f*abs(Balls[m].velocity[0]);
-        }
-        else if (Balls[m].position[1] - Balls[m].mass <= 0){
-            Balls[m].velocity[1] = abs(Balls[m].velocity[1]);
-        }
-        else if (Balls[m].position[1] + Balls[m].mass >= 600) {
-            Balls[m].velocity[1] = -1.0f*abs(Balls[m].velocity[1]);
-        }
+    if(WIDTH<A.sx+A.r){
+        A.vx*=-1.0f;
+        A.sx=WIDTH-A.r;
+    }
+    if(A.sy-A.r<0){
+        A.vy*=-1.0f;
+        A.sy=A.r;
+    }
+    if(HEIGHT<A.sy+A.r){
+        A.vy*=-1.0f;
+        A.sy=HEIGHT-A.r;
+    }
+}
+void oneDimension(float &v1,float &v2, float m1,float m2){
+    float vtem=v1;
+    v1=(vtem*(m1-m2)+2*m2*v2)/(m1+m2);
+    v2=(v2*(m2-m1)+2*m1*vtem)/(m1+m2);
+}
+class vec2{
+public:
+    float x,y;
+    vec2(float x, float y){
+        this->x=x;
+        this->y=y;
+    }
+    float magnitude(){
+        return sqrt(this->x*this->x + this->y*this->y);
+    }
+};
+
+void collideObj(myObj &A, myObj &B){
+    vec2 en=vec2(B.sx-A.sx, B.sy-A.sy);//vector between A to B
+    float mag=en.magnitude();
+    en.x/=mag;
+    en.y/=mag;
+    vec2 et=vec2(-en.y,en.x);
+    //cout<<"Debug "<<et.magnitude()<<", "<<et.magnitude()<<endl;
+    //en=[nx,ny] and et=[tx,ty] are now unit vectors
+    //for normal and tangent directions
+    if(mag < A.r+B.r){
+        B.sx+= (A.r+B.r-mag)*en.x;
+        A.sx-= (A.r+B.r-mag)*en.x;
+        B.sy+= (A.r+B.r-mag)*en.y;
+        A.sy-= (A.r+B.r-mag)*en.y;
+        float vnA=A.vx*en.x + A.vy*en.y;
+        float vtA=A.vx*et.x + A.vy*et.y;
+        float vnB=B.vx*en.x + B.vy*en.y;
+        float vtB=B.vx*et.x + B.vy*et.y;
+        oneDimension(vnA,vnB,A.m,B.m);
+        A.vx=vnA*en.x+vtA*et.x;
+        A.vy=vnA*en.y+vtA*et.y;
+        B.vx=vnB*en.x+vtB*et.x;
+        B.vy=vnB*en.y+vtB*et.y;
     }
 }
 
-void renderScene() {
+void displayCB(){
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0.0, 1000.0, 600.0, 0.0);
-
-    srand(time(NULL));
-    if (once == true) {
-        for (short i = 0; i < 10; ++i) {
-            Balls[i].mass = rand() % 31 + 10;
-            Balls[i].position[0] = rand() % (501 - Balls[i].mass);
-            Balls[i].position[1] = rand() % (501 - Balls[i].mass);
-            short random = rand() % 2;
-            Balls[i].velocity[0] = ((rand() % 11) + 1) / 10.0f;
-            Balls[i].velocity[1] = ((rand() % 11) + 1) / 10.0f;
-            Balls[i].color[0] = (rand() % 256) / 255.0f;
-            Balls[i].color[1] = (rand() % 256) / 255.0f;
-            Balls[i].color[2] = (rand() % 256) / 255.0f;
+    for(int i=0; i<N-1; i++) {
+        for(int j=i+1; j<N; j++){
+            collideObj(balls[i],balls[j]);
         }
-        once = false;
     }
-    for (short i = 0; i < 10; ++i) {
-        glColor3f(Balls[i].color[0], Balls[i].color[1], Balls[i].color[2]);
-        moveCircle(Balls[i], i);
-        drawCircle(Balls[i].position[0], Balls[i].position[1], Balls[i].mass);
-        if(i==0)
-            cout << "Ball" << i << " Mass:" << Balls[i].mass << " Position: " << Balls[i].position[0] << ", " << Balls[i].position[1] << " Velocity: " << Balls[i].velocity[0] << ", " << Balls[i].velocity[1] << endl;
+    for(int i=0; i<N; i++) {
+        collideWall(balls[i]);
+        balls[i].update();
+        balls[i].draw();
+        //balls[i].print();
     }
     glFlush();
     glutPostRedisplay();
 }
 
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(1000, 600);
+    glutInitWindowSize(WIDTH,HEIGHT);
     glutCreateWindow("Collision");
-    glutDisplayFunc(renderScene);
+    glutDisplayFunc(displayCB);
     glMatrixMode(GL_MODELVIEW);
+    init();
     glutMainLoop();
     return 0;
 }
