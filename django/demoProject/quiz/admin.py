3d1d756bf77074b1e1ec72b0fec929884fe313a4
wasit7 <wasit7@gmail.com>
2016-08-26 11:50:14 +0700

    adding demoProject for Pieng

---
+++ django/demoProject/quiz/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import Questions
+class QuestionsAdmin(admin.ModelAdmin):
+    fields = ('question','a','b','c','d','anwser')
+    list_filter=('question','a','b','c','d','anwser')
+admin.site.register(Questions, QuestionsAdmin)
