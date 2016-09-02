853fcc56714853e6a5aba571ea6de939b7db29c8
wasit7 <wasit7@gmail.com>
2016-09-02 12:17:45 +0700

    week03 tutorial for views.py

--- django/Pieng/myclass/myquiz/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from models import Questions
-class QuestionsAdmin(admin.ModelAdmin):
-    fields = ('question','a','b','c','d','anwser')
-    list_filter=('question','a','b','c','d','anwser')
-    list_display=('question','a','b','c','d','anwser')
-
-admin.site.register(Questions, QuestionsAdmin)
